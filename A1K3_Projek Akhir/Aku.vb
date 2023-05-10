8fe894ff07ac2671086467ddf89220a224fa484a
Rismayanti1212 <chimaa1204@gmail.com>
2023-05-10 23:29:08 +0800

    hapus aku

--- A1K3_Projek Akhir/Aku.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Aku
-    Private Sub Form8_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
-
-    End Sub
-End Class
