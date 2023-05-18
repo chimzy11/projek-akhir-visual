c90e7da305f5182e806e3bc41196db5ec993cb3e
NurmedinaMaulidiah <nurmedinamaulidiah@gmail.com>
2023-05-18 13:02:38 +0800

    tambah form musikal

--- A1K3_Projek Akhir/formMusikal.Designer.vb
+++ A1K3_Projek Akhir/formMusikal.Designer.vb
@@ -25,6 +25,15 @@ Partial Class formMusikal
         Me.Label1 = New System.Windows.Forms.Label()
         Me.txtCari = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.lblJudul = New System.Windows.Forms.Label()
+        Me.lblHarga = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
@@ -32,7 +41,7 @@ Partial Class formMusikal
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Bernard MT Condensed", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(132, Byte), Integer), CType(CType(123, Byte), Integer), CType(CType(112, Byte), Integer))
-        Me.Label1.Location = New System.Drawing.Point(128, 101)
+        Me.Label1.Location = New System.Drawing.Point(131, 109)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(160, 31)
         Me.Label1.TabIndex = 0
@@ -44,7 +53,7 @@ Partial Class formMusikal
         Me.txtCari.Location = New System.Drawing.Point(134, 151)
         Me.txtCari.Multiline = True
         Me.txtCari.Name = "txtCari"
-        Me.txtCari.Size = New System.Drawing.Size(400, 29)
+        Me.txtCari.Size = New System.Drawing.Size(400, 32)
         Me.txtCari.TabIndex = 1
         '
         'Button1
@@ -59,12 +68,76 @@ Partial Class formMusikal
         Me.Button1.TabIndex = 2
         Me.Button1.UseVisualStyleBackColor = False
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft YaHei UI", 14.25!, CType(((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic) _
+                Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(132, Byte), Integer), CType(CType(123, Byte), Integer), CType(CType(112, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(368, 114)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(90, 26)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Tanggal"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(132, Byte), Integer), CType(CType(123, Byte), Integer), CType(CType(112, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Location = New System.Drawing.Point(142, 217)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(184, 167)
+        Me.Panel1.TabIndex = 5
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(21, 16)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(139, 135)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(185, Byte), Integer), CType(CType(174, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.Panel2.Controls.Add(Me.lblHarga)
+        Me.Panel2.Controls.Add(Me.lblJudul)
+        Me.Panel2.Location = New System.Drawing.Point(142, 390)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(184, 70)
+        Me.Panel2.TabIndex = 6
+        '
+        'lblJudul
+        '
+        Me.lblJudul.AutoSize = True
+        Me.lblJudul.Font = New System.Drawing.Font("Stencil", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblJudul.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblJudul.Location = New System.Drawing.Point(53, 6)
+        Me.lblJudul.Name = "lblJudul"
+        Me.lblJudul.Size = New System.Drawing.Size(73, 22)
+        Me.lblJudul.TabIndex = 0
+        Me.lblJudul.Text = "Judul"
+        '
+        'lblHarga
+        '
+        Me.lblHarga.AutoSize = True
+        Me.lblHarga.Font = New System.Drawing.Font("Microsoft YaHei", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHarga.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lblHarga.Location = New System.Drawing.Point(60, 30)
+        Me.lblHarga.Name = "lblHarga"
+        Me.lblHarga.Size = New System.Drawing.Size(51, 19)
+        Me.lblHarga.TabIndex = 1
+        Me.lblHarga.Text = "Harga"
+        '
         'formMusikal
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(221, Byte), Integer), CType(CType(212, Byte), Integer), CType(CType(199, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(897, 340)
+        Me.ClientSize = New System.Drawing.Size(897, 504)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.txtCari)
         Me.Controls.Add(Me.Label1)
@@ -72,6 +145,10 @@ Partial Class formMusikal
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "formMusikal"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -80,4 +157,10 @@ Partial Class formMusikal
     Friend WithEvents Label1 As Label
     Friend WithEvents txtCari As TextBox
     Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents lblHarga As Label
+    Friend WithEvents lblJudul As Label
 End Class
