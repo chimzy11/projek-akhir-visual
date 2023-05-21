cf5ecb3c9890a5a5e33a0abea62a959495dd3005
Rismayanti1212 <chimaa1204@gmail.com>
2023-05-22 03:06:36 +0800

    tambah cekbox

--- A1K3_Projek Akhir/formPesan.Designer.vb
+++ A1K3_Projek Akhir/formPesan.Designer.vb
@@ -28,18 +28,17 @@ Partial Class formPesan
         Me.pKembali = New System.Windows.Forms.PictureBox()
         Me.lJudul = New System.Windows.Forms.Label()
         Me.Panel2 = New System.Windows.Forms.Panel()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.tBanyakTiket = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.lTotalHarga = New System.Windows.Forms.Label()
         Me.bTambahJadwal = New System.Windows.Forms.Button()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.lUsername = New System.Windows.Forms.Label()
         Me.Panel1.SuspendLayout()
         CType(Me.pKembali, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.Panel2.SuspendLayout()
         Me.Panel3.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -47,7 +46,7 @@ Partial Class formPesan
         '
         Me.FlowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(250, Byte), Integer), CType(CType(246, Byte), Integer), CType(CType(243, Byte), Integer))
         Me.FlowLayoutPanel1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(250, Byte), Integer), CType(CType(246, Byte), Integer), CType(CType(243, Byte), Integer))
-        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(173, 111)
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(213, 112)
         Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
         Me.FlowLayoutPanel1.Size = New System.Drawing.Size(329, 33)
         Me.FlowLayoutPanel1.TabIndex = 0
@@ -60,7 +59,7 @@ Partial Class formPesan
         Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
         Me.Panel1.Location = New System.Drawing.Point(0, 0)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(637, 76)
+        Me.Panel1.Size = New System.Drawing.Size(800, 76)
         Me.Panel1.TabIndex = 1
         '
         'pKembali
@@ -79,7 +78,7 @@ Partial Class formPesan
         Me.lJudul.AutoSize = True
         Me.lJudul.Font = New System.Drawing.Font("Stencil", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lJudul.ForeColor = System.Drawing.Color.FromArgb(CType(CType(221, Byte), Integer), CType(CType(212, Byte), Integer), CType(CType(199, Byte), Integer))
-        Me.lJudul.Location = New System.Drawing.Point(236, 17)
+        Me.lJudul.Location = New System.Drawing.Point(275, 17)
         Me.lJudul.Name = "lJudul"
         Me.lJudul.Size = New System.Drawing.Size(197, 38)
         Me.lJudul.TabIndex = 108
@@ -89,46 +88,34 @@ Partial Class formPesan
         'Panel2
         '
         Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(132, Byte), Integer), CType(CType(123, Byte), Integer), CType(CType(112, Byte), Integer))
-        Me.Panel2.Controls.Add(Me.Label1)
         Me.Panel2.Location = New System.Drawing.Point(23, 171)
         Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(607, 374)
+        Me.Panel2.Size = New System.Drawing.Size(757, 374)
         Me.Panel2.TabIndex = 2
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.ForeColor = System.Drawing.Color.Red
-        Me.Label1.Location = New System.Drawing.Point(137, 52)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(290, 20)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "*nanti sini codingan checkbox buat kursi"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft YaHei UI", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(132, Byte), Integer), CType(CType(123, Byte), Integer), CType(CType(112, Byte), Integer))
-        Me.Label4.Location = New System.Drawing.Point(19, 576)
+        Me.Label4.Location = New System.Drawing.Point(19, 582)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(114, 22)
         Me.Label4.TabIndex = 48
-        Me.Label4.Text = "Banyak Kursi"
+        Me.Label4.Text = "Jumlah Tiket"
         '
-        'TextBox1
+        'tBanyakTiket
         '
-        Me.TextBox1.AcceptsReturn = True
-        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(185, Byte), Integer), CType(CType(174, Byte), Integer), CType(CType(169, Byte), Integer))
-        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.TextBox1.Enabled = False
-        Me.TextBox1.Font = New System.Drawing.Font("Microsoft YaHei UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(132, Byte), Integer), CType(CType(123, Byte), Integer), CType(CType(112, Byte), Integer))
-        Me.TextBox1.Location = New System.Drawing.Point(23, 609)
-        Me.TextBox1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(183, 26)
-        Me.TextBox1.TabIndex = 49
+        Me.tBanyakTiket.AcceptsReturn = True
+        Me.tBanyakTiket.BackColor = System.Drawing.Color.FromArgb(CType(CType(185, Byte), Integer), CType(CType(174, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.tBanyakTiket.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.tBanyakTiket.Font = New System.Drawing.Font("Microsoft YaHei UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tBanyakTiket.ForeColor = System.Drawing.Color.FromArgb(CType(CType(132, Byte), Integer), CType(CType(123, Byte), Integer), CType(CType(112, Byte), Integer))
+        Me.tBanyakTiket.Location = New System.Drawing.Point(23, 621)
+        Me.tBanyakTiket.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.tBanyakTiket.Name = "tBanyakTiket"
+        Me.tBanyakTiket.Size = New System.Drawing.Size(183, 26)
+        Me.tBanyakTiket.TabIndex = 49
         '
         'Button1
         '
@@ -138,7 +125,7 @@ Partial Class formPesan
         Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button1.Font = New System.Drawing.Font("Stencil", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(250, Byte), Integer), CType(CType(246, Byte), Integer), CType(CType(243, Byte), Integer))
-        Me.Button1.Location = New System.Drawing.Point(18, 600)
+        Me.Button1.Location = New System.Drawing.Point(18, 612)
         Me.Button1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(194, 43)
@@ -148,14 +135,26 @@ Partial Class formPesan
         'Panel3
         '
         Me.Panel3.BackColor = System.Drawing.Color.FromArgb(CType(CType(132, Byte), Integer), CType(CType(123, Byte), Integer), CType(CType(112, Byte), Integer))
+        Me.Panel3.Controls.Add(Me.lTotalHarga)
         Me.Panel3.Controls.Add(Me.bTambahJadwal)
         Me.Panel3.Controls.Add(Me.Label6)
         Me.Panel3.Controls.Add(Me.Label5)
         Me.Panel3.Controls.Add(Me.lUsername)
         Me.Panel3.Location = New System.Drawing.Point(0, 674)
         Me.Panel3.Name = "Panel3"
-        Me.Panel3.Size = New System.Drawing.Size(683, 110)
+        Me.Panel3.Size = New System.Drawing.Size(803, 110)
         Me.Panel3.TabIndex = 51
+        '
+        'lTotalHarga
+        '
+        Me.lTotalHarga.AutoSize = True
+        Me.lTotalHarga.Font = New System.Drawing.Font("Microsoft YaHei UI", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lTotalHarga.ForeColor = System.Drawing.Color.FromArgb(CType(CType(250, Byte), Integer), CType(CType(246, Byte), Integer), CType(CType(243, Byte), Integer))
+        Me.lTotalHarga.Location = New System.Drawing.Point(368, 54)
+        Me.lTotalHarga.Name = "lTotalHarga"
+        Me.lTotalHarga.Size = New System.Drawing.Size(72, 27)
+        Me.lTotalHarga.TabIndex = 56
+        Me.lTotalHarga.Text = "50000"
         '
         'bTambahJadwal
         '
@@ -166,11 +165,11 @@ Partial Class formPesan
         Me.bTambahJadwal.Font = New System.Drawing.Font("Microsoft YaHei UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.bTambahJadwal.ForeColor = System.Drawing.Color.FromArgb(CType(CType(250, Byte), Integer), CType(CType(246, Byte), Integer), CType(CType(243, Byte), Integer))
         Me.bTambahJadwal.Image = CType(resources.GetObject("bTambahJadwal.Image"), System.Drawing.Image)
-        Me.bTambahJadwal.Location = New System.Drawing.Point(441, 0)
+        Me.bTambahJadwal.Location = New System.Drawing.Point(590, 2)
         Me.bTambahJadwal.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.bTambahJadwal.Name = "bTambahJadwal"
         Me.bTambahJadwal.Padding = New System.Windows.Forms.Padding(4, 15, 0, 0)
-        Me.bTambahJadwal.Size = New System.Drawing.Size(223, 98)
+        Me.bTambahJadwal.Size = New System.Drawing.Size(213, 98)
         Me.bTambahJadwal.TabIndex = 55
         Me.bTambahJadwal.Text = "Checkout"
         Me.bTambahJadwal.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
@@ -181,7 +180,7 @@ Partial Class formPesan
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Microsoft YaHei UI", 8.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label6.ForeColor = System.Drawing.Color.FromArgb(CType(CType(250, Byte), Integer), CType(CType(246, Byte), Integer), CType(CType(243, Byte), Integer))
-        Me.Label6.Location = New System.Drawing.Point(223, 20)
+        Me.Label6.Location = New System.Drawing.Point(329, 32)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(106, 22)
         Me.Label6.TabIndex = 54
@@ -192,7 +191,7 @@ Partial Class formPesan
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Microsoft YaHei UI", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label5.ForeColor = System.Drawing.Color.FromArgb(CType(CType(250, Byte), Integer), CType(CType(246, Byte), Integer), CType(CType(243, Byte), Integer))
-        Me.Label5.Location = New System.Drawing.Point(222, 42)
+        Me.Label5.Location = New System.Drawing.Point(317, 54)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(45, 27)
         Me.Label5.TabIndex = 53
@@ -216,11 +215,11 @@ Partial Class formPesan
         Me.AutoSize = True
         Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(221, Byte), Integer), CType(CType(212, Byte), Integer), CType(CType(199, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(637, 777)
+        Me.ClientSize = New System.Drawing.Size(800, 777)
         Me.ControlBox = False
         Me.Controls.Add(Me.Panel3)
         Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.tBanyakTiket)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Panel1)
@@ -233,8 +232,6 @@ Partial Class formPesan
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
         CType(Me.pKembali, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.Panel2.ResumeLayout(False)
-        Me.Panel2.PerformLayout()
         Me.Panel3.ResumeLayout(False)
         Me.Panel3.PerformLayout()
         Me.ResumeLayout(False)
@@ -245,9 +242,8 @@ Partial Class formPesan
     Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
     Friend WithEvents Panel1 As Panel
     Friend WithEvents Panel2 As Panel
-    Friend WithEvents Label1 As Label
     Friend WithEvents Label4 As Label
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents tBanyakTiket As TextBox
     Friend WithEvents Button1 As Button
     Friend WithEvents lJudul As Label
     Friend WithEvents Panel3 As Panel
@@ -256,4 +252,5 @@ Partial Class formPesan
     Friend WithEvents Label6 As Label
     Friend WithEvents bTambahJadwal As Button
     Friend WithEvents pKembali As PictureBox
+    Friend WithEvents lTotalHarga As Label
 End Class
--- A1K3_Projek Akhir/formPesan.vb
+++ A1K3_Projek Akhir/formPesan.vb
@@ -4,6 +4,11 @@ Imports System.IO
 Imports System.Windows.Forms.DataVisualization.Charting
 Public Class formPesan
 
+    Dim Total As Integer = 0
+    Dim HargaPerTiket As Integer = 0
+    Dim jumlahTiket As Integer = 0
+    Dim selectedSeats As New List(Of CheckBox)()
+
     Sub PilihanKursi()
         Dim queryString As String = "SELECT * FROM JadwalTeater WHERE judul = @judul"
 
@@ -12,30 +17,30 @@ Public Class formPesan
         RD = CMD.ExecuteReader()
 
         If RD.Read() Then
-
-            Dim jumlahTiket As Integer = RD.GetString(7)
-
+            jumlahTiket = RD.GetInt32(7)
             Dim numCheckboxes As Integer = jumlahTiket
-            Dim checkBoxesPerRow As Integer = 5
-            Dim checkBoxWidth As Integer = 100
-            Dim checkBoxHeight As Integer = 20
-            Dim spacing As Integer = 10
+            Dim checkBoxesPerRow As Integer = 10
+            Dim checkBoxWidth As Integer = 40
+            Dim checkBoxHeight As Integer = 30
 
-            Dim nama As Char = "A"c ' Starting character
+            Dim nama As Char = "A"c
 
             For i As Integer = 0 To numCheckboxes - 1
                 Dim checkBox As New CheckBox()
-                checkBox.Name = "checkBox" & i.ToString()
+                checkBox.Name = "A" & i.ToString()
                 checkBox.Text = nama.ToString() & i.ToString()
-                Dim row As Integer = i \ checkBoxesPerRow ' Calculate row
-                Dim col As Integer = i Mod checkBoxesPerRow ' Calculate column
-                Dim x As Integer = 20 + col * (checkBoxWidth + spacing) ' Calculate X position
-                Dim y As Integer = 50 + row * (checkBoxHeight + spacing) ' Calculate Y position
+                checkBox.AutoSize = True ' Mengatur AutoSize ke True
+                Dim row As Integer = i \ checkBoxesPerRow
+                Dim col As Integer = i Mod checkBoxesPerRow
+                Dim x As Integer = 33 + col * (checkBoxWidth + 5)
+                Dim y As Integer = 43 + row * (checkBoxHeight + 5)
                 checkBox.Location = New Point(x, y)
+                checkBox.Width = checkBoxWidth ' Mengatur lebar CheckBox
+                checkBox.Enabled = False
+                AddHandler checkBox.CheckedChanged, AddressOf CheckBox_CheckedChanged ' Menambahkan event handler CheckedChanged
                 Panel2.Controls.Add(checkBox)
 
-                ' Update character for every group of 5 CheckBoxes
-                If (i + 1) Mod 5 = 0 Then
+                If (i + 1) Mod 10 = 0 Then
                     If nama < "Z"c Then
                         nama = Chr(Asc(nama) + 1)
                     End If
@@ -45,25 +50,29 @@ Public Class formPesan
 
         RD.Close()
     End Sub
-
-
     Private Sub formPesan_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Call KoneksiDatabase()
         PilihanKursi()
-        Dim queryString As String = "SELECT * FROM JadwalTeater WHERE judul ='" & PopUpDataJadwal.cJudul.Text & "'"
+
+        tBanyakTiket.Focus()
+
+        Dim queryString As String = "SELECT * FROM JadwalTeater WHERE judul = @judul"
 
         CMD = New MySqlCommand(queryString, CONN)
+        CMD.Parameters.AddWithValue("@judul", PopUpDataJadwal.cJudul.Text)
         RD = CMD.ExecuteReader()
 
         If RD.Read() Then
             lJudul.Text = RD.GetString(1)
+            HargaPerTiket = RD.GetInt32(8) ' Simpan harga per tiket dari database
         End If
 
         RD.Close()
 
-        Dim queryString1 As String = "SELECT * FROM akun WHERE username ='" & FLogin.tUsername.Text & "'"
+        Dim queryString1 As String = "SELECT * FROM akun WHERE username = @username"
 
         CMD = New MySqlCommand(queryString1, CONN)
+        CMD.Parameters.AddWithValue("@username", FLogin.tUsername.Text)
         RD = CMD.ExecuteReader()
 
         If RD.Read() Then
@@ -71,6 +80,49 @@ Public Class formPesan
         End If
 
         RD.Close()
-
     End Sub
+
+    Private Sub tBanyakTiket_TextChanged(sender As Object, e As EventArgs) Handles tBanyakTiket.TextChanged
+        Dim BanyakTiket As Integer = 0
+
+        If Integer.TryParse(tBanyakTiket.Text, BanyakTiket) Then
+            If (BanyakTiket > jumlahTiket) Then
+                MessageBox.Show("Banyak tiket tidak boleh lebih dari banyak kursi yang tersisa", "Perhatian", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                tBanyakTiket.Focus()
+                Return
+            End If
+
+            For Each checkBox As CheckBox In Panel2.Controls.OfType(Of CheckBox)()
+                checkBox.Enabled = True ' Mengaktifkan semua cekbox
+            Next
+
+            Total = BanyakTiket * HargaPerTiket ' Hitung total harga
+            lTotalHarga.Text = Total.ToString()
+        Else
+            lTotalHarga.Text = "0" ' Tampilkan 0 jika input tidak valid
+        End If
+
+        If BanyakTiket = 0 Then
+            For Each checkBox As CheckBox In Panel2.Controls.OfType(Of CheckBox)()
+                checkBox.Enabled = False ' Mengaktifkan semua cekbox
+                tBanyakTiket.Focus()
+            Next
+        End If
+    End Sub
+
+    Private Sub CheckBox_CheckedChanged(sender As Object, e As EventArgs)
+        Dim checkBox As CheckBox = CType(sender, CheckBox)
+
+        If checkBox.Checked Then
+            If selectedSeats.Count >= jumlahTiket Then
+                MessageBox.Show("Jumlah tiket yang dipilih melebihi jumlah kursi yang tersedia", "Perhatian", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                checkBox.Checked = False
+            Else
+                selectedSeats.Add(checkBox)
+            End If
+        Else
+            selectedSeats.Remove(checkBox)
+        End If
+    End Sub
+
 End Class
