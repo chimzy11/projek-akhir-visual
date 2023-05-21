8daeb4e1f0385cbc8d5855de79c17da7ebf2df9d
NurmedinaMaulidiah <nurmedinamaulidiah@gmail.com>
2023-05-21 09:54:48 +0800

    form transaksi fix desain

--- A1K3_Projek Akhir/formTransaksi.Designer.vb
+++ A1K3_Projek Akhir/formTransaksi.Designer.vb
@@ -23,85 +23,73 @@ Partial Class formTransaksi
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
-        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
-        Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(formTransaksi))
-        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
-        Me.Label9 = New System.Windows.Forms.Label()
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label9 = New System.Windows.Forms.Label()
         Me.txtSearch = New System.Windows.Forms.TextBox()
         Me.btnSearch = New System.Windows.Forms.Button()
-        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'DataGridView2
-        '
-        Me.DataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
-        Me.DataGridView2.BackgroundColor = System.Drawing.Color.White
-        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
-        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control
-        DataGridViewCellStyle1.Font = New System.Drawing.Font("Segoe UI Semibold", 10.0!, System.Drawing.FontStyle.Bold)
-        DataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black
-        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
-        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
-        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
-        Me.DataGridView2.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
-        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
-        DataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window
-        DataGridViewCellStyle2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        DataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText
-        DataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(CType(CType(132, Byte), Integer), CType(CType(123, Byte), Integer), CType(CType(112, Byte), Integer))
-        DataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText
-        DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
-        Me.DataGridView2.DefaultCellStyle = DataGridViewCellStyle2
-        Me.DataGridView2.GridColor = System.Drawing.SystemColors.ControlDarkDark
-        Me.DataGridView2.Location = New System.Drawing.Point(148, 229)
-        Me.DataGridView2.Name = "DataGridView2"
-        DataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
-        DataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control
-        DataGridViewCellStyle3.Font = New System.Drawing.Font("Segoe UI Semibold", 10.0!, System.Drawing.FontStyle.Bold)
-        DataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(250, Byte), Integer), CType(CType(246, Byte), Integer), CType(CType(243, Byte), Integer))
-        DataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(CType(CType(132, Byte), Integer), CType(CType(123, Byte), Integer), CType(CType(112, Byte), Integer))
-        DataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText
-        DataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
-        Me.DataGridView2.RowHeadersDefaultCellStyle = DataGridViewCellStyle3
-        Me.DataGridView2.RowHeadersWidth = 62
-        Me.DataGridView2.Size = New System.Drawing.Size(1073, 150)
-        Me.DataGridView2.TabIndex = 2
-        '
-        'Label9
-        '
-        Me.Label9.AutoSize = True
-        Me.Label9.BackColor = System.Drawing.Color.FromArgb(CType(CType(221, Byte), Integer), CType(CType(212, Byte), Integer), CType(CType(199, Byte), Integer))
-        Me.Label9.Font = New System.Drawing.Font("Elephant", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label9.ForeColor = System.Drawing.Color.FromArgb(CType(CType(132, Byte), Integer), CType(CType(123, Byte), Integer), CType(CType(112, Byte), Integer))
-        Me.Label9.Location = New System.Drawing.Point(370, 26)
-        Me.Label9.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(285, 31)
-        Me.Label9.TabIndex = 25
-        Me.Label9.Text = "Data Transaksi User"
         '
         'Panel1
         '
         Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(221, Byte), Integer), CType(CType(212, Byte), Integer), CType(CType(199, Byte), Integer))
-        Me.Panel1.Controls.Add(Me.Label9)
         Me.Panel1.Controls.Add(Me.Panel2)
-        Me.Panel1.Location = New System.Drawing.Point(139, 137)
+        Me.Panel1.Controls.Add(Me.Label9)
+        Me.Panel1.Location = New System.Drawing.Point(120, 150)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(2)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(1088, 403)
-        Me.Panel1.TabIndex = 26
+        Me.Panel1.Size = New System.Drawing.Size(1116, 975)
+        Me.Panel1.TabIndex = 30
         '
         'Panel2
         '
         Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(132, Byte), Integer), CType(CType(123, Byte), Integer), CType(CType(112, Byte), Integer))
-        Me.Panel2.Location = New System.Drawing.Point(0, 79)
+        Me.Panel2.Controls.Add(Me.DataGridView1)
+        Me.Panel2.Location = New System.Drawing.Point(27, 64)
+        Me.Panel2.Margin = New System.Windows.Forms.Padding(2)
         Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(1088, 324)
-        Me.Panel2.TabIndex = 0
+        Me.Panel2.Size = New System.Drawing.Size(1061, 975)
+        Me.Panel2.TabIndex = 28
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.BackgroundColor = System.Drawing.Color.White
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(15, 10)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(2)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowHeadersWidth = 62
+        Me.DataGridView1.RowTemplate.Height = 28
+        Me.DataGridView1.Size = New System.Drawing.Size(1032, 975)
+        Me.DataGridView1.TabIndex = 24
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Elephant", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.ForeColor = System.Drawing.Color.FromArgb(CType(CType(132, Byte), Integer), CType(CType(123, Byte), Integer), CType(CType(112, Byte), Integer))
+        Me.Label9.Location = New System.Drawing.Point(419, 21)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(285, 31)
+        Me.Label9.TabIndex = 24
+        Me.Label9.Text = "Data Transaksi User"
         '
         'txtSearch
         '
@@ -109,22 +97,22 @@ Partial Class formTransaksi
         Me.txtSearch.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.txtSearch.Font = New System.Drawing.Font("Microsoft YaHei UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txtSearch.ForeColor = System.Drawing.Color.FromArgb(CType(CType(132, Byte), Integer), CType(CType(123, Byte), Integer), CType(CType(112, Byte), Integer))
-        Me.txtSearch.Location = New System.Drawing.Point(916, 79)
+        Me.txtSearch.Location = New System.Drawing.Point(926, 107)
         Me.txtSearch.Margin = New System.Windows.Forms.Padding(2, 1, 2, 1)
         Me.txtSearch.Name = "txtSearch"
         Me.txtSearch.Size = New System.Drawing.Size(287, 17)
-        Me.txtSearch.TabIndex = 28
+        Me.txtSearch.TabIndex = 29
         Me.txtSearch.Text = "Search"
         '
         'btnSearch
         '
         Me.btnSearch.BackColor = System.Drawing.Color.FromArgb(CType(CType(221, Byte), Integer), CType(CType(212, Byte), Integer), CType(CType(199, Byte), Integer))
         Me.btnSearch.Enabled = False
-        Me.btnSearch.Location = New System.Drawing.Point(908, 72)
+        Me.btnSearch.Location = New System.Drawing.Point(922, 100)
         Me.btnSearch.Margin = New System.Windows.Forms.Padding(2, 1, 2, 1)
         Me.btnSearch.Name = "btnSearch"
         Me.btnSearch.Size = New System.Drawing.Size(317, 33)
-        Me.btnSearch.TabIndex = 27
+        Me.btnSearch.TabIndex = 28
         Me.btnSearch.Text = "" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         Me.btnSearch.UseVisualStyleBackColor = False
         '
@@ -133,27 +121,27 @@ Partial Class formTransaksi
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(185, Byte), Integer), CType(CType(174, Byte), Integer), CType(CType(169, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(1104, 450)
+        Me.ClientSize = New System.Drawing.Size(1208, 450)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.txtSearch)
         Me.Controls.Add(Me.btnSearch)
-        Me.Controls.Add(Me.DataGridView2)
-        Me.Controls.Add(Me.Panel1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "formTransaksi"
         Me.Text = "formTransaksi"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
-        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents DataGridView2 As DataGridView
-    Friend WithEvents Label9 As Label
     Friend WithEvents Panel1 As Panel
     Friend WithEvents Panel2 As Panel
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label9 As Label
     Friend WithEvents txtSearch As TextBox
     Friend WithEvents btnSearch As Button
 End Class
--- A1K3_Projek Akhir/formTransaksi.vb
+++ A1K3_Projek Akhir/formTransaksi.vb
@@ -1,69 +1,27 @@
 ﻿Public Class formTransaksi
-    Private Sub btnClose_Click(sender As Object, e As EventArgs) 
-        Me.Close()
+    Private Sub GridDataTransaksi()
+        DataGridView1.Columns.Clear() ' Hapus semua kolom sebelum menambahkan kolom baru
+
+        DataGridView1.Columns.Add("NamaPengirim", "Nama Pengirim")
+        DataGridView1.Columns.Add("NamaPenerima", "Nama Penerima")
+        DataGridView1.Columns.Add("JumlahTiket", "Jumlah Tiket")
+        DataGridView1.Columns.Add("JudulTeater", "Judul Teater")
+        DataGridView1.Columns.Add("NoRekeningPengirim", "No Rekening Pengirim")
+        DataGridView1.Columns.Add("NoRekeningPenerima", "No Rekening Penerima")
+        DataGridView1.Columns.Add("Total", "Total")
+
+        DataGridView1.Columns(0).Width = 140
+        DataGridView1.Columns(1).Width = 140
+        DataGridView1.Columns(2).Width = 70
+        DataGridView1.Columns(3).Width = 160
+        DataGridView1.Columns(4).Width = 180
+        DataGridView1.Columns(5).Width = 190
+        DataGridView1.Columns(6).Width = 90
     End Sub
 
-    Private Sub btnMax_Click(sender As Object, e As EventArgs) 
-        If Me.WindowState = FormWindowState.Normal Then
-            Me.WindowState = FormWindowState.Maximized
-        Else
-            Me.WindowState = FormWindowState.Normal
-        End If
 
-    End Sub
-    Private Sub DataGridView2_CellFormatting(sender As Object, e As DataGridViewCellFormattingEventArgs) 
-        If e.RowIndex Mod 2 = 0 Then
-            ' Mengatur warna latar belakang baris untuk baris genap
-            e.CellStyle.BackColor = Color.FromArgb(250, 246, 243)
-        Else
-            ' Mengatur warna latar belakang baris untuk baris ganjil
-            e.CellStyle.BackColor = Color.FromArgb(185, 174, 169)
-        End If
-    End Sub
 
     Private Sub formTransaksi_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         GridDataTransaksi()
-        DataGridView2.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(185, 174, 169)
-        DataGridView2.EnableHeadersVisualStyles = False
-
-        AddHandler DataGridView2.CellFormatting, AddressOf DataGridView2_CellFormatting
-        AutoResizeColumns()
-    End Sub
-    Private Sub AutoResizeColumns()
-        For Each column As DataGridViewColumn In DataGridView2.Columns
-            column.AutoSizeMode = DataGridViewAutoSizeColumnMode.ColumnHeader
-        Next
-    End Sub
-
-    Sub GridDataTransaksi()
-        DataGridView2.Columns(0).Width = 90
-        DataGridView2.Columns(1).Width = 90
-        DataGridView2.Columns(2).Width = 80
-        DataGridView2.Columns(3).Width = 150
-        DataGridView2.Columns(4).Width = 150
-        DataGridView2.Columns(5).Width = 150
-        DataGridView2.Columns(6).Width = 80
-
-        DataGridView2.Columns(0).HeaderText = "Nama Pengirim"
-        DataGridView2.Columns(1).HeaderText = "Nama Penerima"
-        DataGridView2.Columns(2).HeaderText = "Jumlah Tiket"
-        DataGridView2.Columns(3).HeaderText = "Judul Teater"
-        DataGridView2.Columns(4).HeaderText = "No Rekening Pengirim"
-        DataGridView2.Columns(5).HeaderText = "No Rekening Penerima"
-        DataGridView2.Columns(6).HeaderText = "No Rekening Pengirim"
-
-    End Sub
-
-
-    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) 
-
-    End Sub
-
-    Private Sub DataGridView2_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) 
-
-    End Sub
-
-    Private Sub Panel1_Paint_1(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
-
     End Sub
 End Class
